eb0b2e407035c69d823fe2f3d469ee7b22562e65
rodell <ryanodell2668@gmail.com>
2022-12-20 16:53:57 -0500

    Added header to recipe project

---
+++ recipe-collector-project/recipe-website-project/src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <h1>Recipe Warehouse</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style>
+.header {
+  box-shadow: 0 5px 30px rgba(0, 0, 0, 0.30);
+  margin: 0.5rem;
+  border-radius: 8px;
+  padding: 0.5rem;
+  background-color: #0f936c;
+  color: white;
+  text-align: center;
+}
+</style>
