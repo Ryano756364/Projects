65e5c7a8f963042aa8ade56c790b8c722cd19904
rodell <ryanodell2668@gmail.com>
2022-12-21 09:19:28 -0500

    Header added

--- recipe-warehouse-project/src/views/HomeView.vue
+++ recipe-warehouse-project/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Recipe Warehouse</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
   }
-}
 </script>
+
+<style>
+  .home {
+    box-shadow: 0 5px 30px rgba(0, 0, 0, 0.30);
+    margin: 0.5rem 0.5rem 2.5rem 0.5rem;
+    border-radius: 8px;
+    padding: 0.5rem;
+    background-color: #0f936c;
+    color: white;
+    text-align: center;
+  }
+</style>
