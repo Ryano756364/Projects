327823a35aa1de8d9e4d3c491021b1c01ebbb5d7
rodell <ryanodell2668@gmail.com>
2023-01-03 10:42:14 -0500

    Menu setup complete

--- healthy_recipe_menu_project/healthy_recipe.py
+++ healthy_recipe_menu_project/healthy_recipe.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
-print("test")
+recipe_book = [
+    ("Chocolate Chip Cookies", "10,000 Calories", 2022,
+     [
+         (2, "Tubs of Butter"),
+         (2, "Bags of Chocolate Chips"),
+         (3, "Gallons of Oil"),
+         (3, "Gallons of Milk"),
+         (2, "Pounds of Love")
+     ]
+     ),
+    ("Buttered Strawberry Toast", "7,000 Calories", 2022,
+     [
+         (3, "Pieces of Toast"),
+         (2, "Sticks of Butter"),
+         (2, "Gallons of Oil"),
+         (4, "Bags of Strawberries"),
+     ]
+     ),
+    ("Healthy Donuts", "6,000 Calories", 2023,
+     [
+         (2, "Tubs of Flour"),
+         (2, "Cups of Water"),
+         (1, "Ultra Healthy Gluten Free Vegan Friendly Oil"),
+         (6, "Tubs of Oil"),
+     ]
+     ),
+    ("Healthy Chocolate Shake", "4,000 Calories", 2022,
+     [
+         (1, "Sugar Free Chocolate Chips"),
+         (1, "Cup of Water"),
+         (1, "Pound of Earth Friendly Flour"),
+         (4, "Tubs of Heavy Cream"),
+     ]
+     ),
+]
